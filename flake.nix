557af0bbcad4d653bec8ca2b6a5c35ca84de7d99
Baraa Homsi <baraa.homsi@proton.me>
2025-04-09 13:52:01 +0000

    feat(flake): removed shell.nix and nixpkgs input

--- flake.nix
+++ flake.nix
@@ -1,22 +1,6 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = { };
+  outputs = _: {
+    nixosModules.default = ./module.nix;
   };
-  outputs = { nixpkgs, ... }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forall = fn: nixpkgs.lib.attrsets.genAttrs systems (system:
-        fn nixpkgs.legacyPackages.${system});
-      call = file: forall (pkgs: {
-        default = pkgs.callPackage file { };
-      });
-    in {
-      nixosModules.default = ./module.nix;
-      devShells = call ./shell.nix;
-    };
 }
